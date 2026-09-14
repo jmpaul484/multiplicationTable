1421216bbded8243492e30e15f3aea6a10c08f9c
jmpau <jmpau@JMP_WORK>
2026-09-14 07:45:23 -0600

    Add project files.

---
+++ multiplicationTable/Program.cs
@@ -0,0 +1,15 @@
+﻿//Jonathan Paul
+//RCET2265
+//Fall 2026
+//
+namespace multiplicationTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.ReadLine();
+        }
+    }
+}
