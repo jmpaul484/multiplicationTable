0ddc959e88d8e92963d95c1604a13e5c23a54286
jmpau <jmpau@JMP_WORK>
2026-09-15 08:06:50 -0600

    Added loops so if the user puts in an invalid input the program will not allow them to continue. Set 12 * 12 as the default. Made the resulting table multiplication rather than addition.

--- multiplicationTable/Program.cs
+++ multiplicationTable/Program.cs
@@ -11,31 +11,53 @@ namespace multiplicationTable
             String userInput1 = "";
             String userInput2 = "";
 
-            int number1 = 7;
-            int number2 = 7;
+            int number1 = 12;
+            int number2 = 12;
 
             bool isValid = false;
 
             int result = 0;
             string output = "";
 
+            while (isValid == false)
+            {
             Console.WriteLine("Welcome, please enter the length of the table");
             userInput1 = Console.ReadLine();
-            if (isValid == false)
-            {
-                Console.WriteLine("Please enter a valid number");
-                userInput1 = Console.ReadLine();
-            }
-            number1 = int.Parse(userInput1);
-            Console.WriteLine("Welcome, please enter the width of the table");
-            userInput2 = Console.ReadLine();
-            number2 = int.Parse(userInput2);
-
-            for (int i = 0; i < number1; i++)
-            {
-                for (int j = 0; j < number2; j++)
+                try
                 {
-                    result = i + j;
+                number1 = int.Parse(userInput1);
+                isValid = true;
+                }
+                catch
+                {
+                    Console.WriteLine("Please enter a valid number");
+                    isValid = false;
+                }
+            }
+
+            isValid = false;
+
+            while (isValid == false)
+            {
+                Console.WriteLine("Welcome, please enter the width of the table");
+                userInput2 = Console.ReadLine();
+                try
+                {
+                    number2 = int.Parse(userInput2);
+                    isValid = true;
+                }
+                catch
+                {
+                    Console.WriteLine("Please enter a valid number");
+                    isValid = false;
+                }
+            }
+
+            for (int i = 1; i <= number1; i++)
+            {
+                for (int j = 1; j <= number2; j++)
+                {
+                    result = i * j;
                     output = $"{result} |";
                     output = output.PadLeft(5);
                     Console.Write(output);
